48bc7adcd6f68d9013ac9b25dcaed2e174e7aafc
mazn74793-png <mazn74793@gmail.com>
2026-01-20 22:47:11 +0200

    Delete supabase.js

--- supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
-
-export const supabase = createClient(
-  "https://rjfkjdjnqqtotvvrmcey.supabase.co",
-  "sb_publishable_xMQrZWmWH2EfnaxcCcbBNA_OpHO66Bp"
-);
